908d0012574d3e18229b625e9f2f92f9b9c7ae77
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-21 00:14:14 +0300

    React #3

--- Hello-React/public/js/app.js
+++ Hello-React/public/js/app.js
@@ -1,5 +1,5 @@
 "use strict";
 
-var template = /*#__PURE__*/React.createElement("h1", null, "My first react app2"); /* bu yazım şeklime JSX : JSON XML denir. */
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "My first react app2"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."))); /* bu yazım şeklime JSX : JSON XML denir. */
 var root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(template);
--- Hello-React/src/app.js
+++ Hello-React/src/app.js
@@ -1,3 +1,13 @@
-var template = <h1>My first react app2</h1> /* bu yazım şeklime JSX : JSON XML denir. */
+var template = 
+<div>
+    <h1>My first react app2</h1>
+    <div>Lorem ipsum dolor sit amet.</div>
+    <ul>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor.</li>
+    </ul>
+</div> /* bu yazım şeklime JSX : JSON XML denir. */
 var root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(template)
