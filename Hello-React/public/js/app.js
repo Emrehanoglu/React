9ee65832a0b4b66b529a89049e7e5152a755282b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-20 23:43:29 +0300

    React #2

---
+++ Hello-React/public/js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var template = /*#__PURE__*/React.createElement("h1", null, "My first react app2"); /* bu yazım şeklime JSX : JSON XML denir. */
+var root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(template);
---
+++ Hello-React/src/app.js
@@ -0,0 +1,3 @@
+var template = <h1>My first react app2</h1> /* bu yazım şeklime JSX : JSON XML denir. */
+var root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(template)
