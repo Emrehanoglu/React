9f259fe0d622fb11fe41ab84294481dc90e92502
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-22 20:01:27 +0300

    React #4

--- Hello-React/public/js/app.js
+++ Hello-React/public/js/app.js
@@ -1,5 +1,56 @@
 "use strict";
 
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "My first react app2"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."))); /* bu yazım şeklime JSX : JSON XML denir. */
 var root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(template);
+var product = [{
+  name: "Iphone11",
+  price: 20000
+}, {
+  name: "Iphone12",
+  price: 30000
+}];
+var selectedProducts = [];
+function productAdd(event, product) {
+  console.log(event.target, product.name);
+  selectedProducts.push(product.name);
+  renderApp();
+}
+function saveProduct(event) {
+  event.preventDefault(); /* submit edilen sayfanın refresh edilmesinin önüne geçer */
+  var pname = event.target.elements.productName.value;
+  var pprice = event.target.elements.productPrice.value;
+  var newProduct = {
+    name: pname,
+    price: pprice
+  };
+  product.push(newProduct);
+  event.target.elements.productName.value = "";
+  event.target.elements.productPrice.value = "";
+  renderApp();
+}
+function renderApp() {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("form", {
+    onSubmit: saveProduct
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "productName",
+    id: "productName"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "productPrice",
+    id: "productPrice"
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Ekle")), /*#__PURE__*/React.createElement("h1", null, "My first react app"), /*#__PURE__*/React.createElement("h2", null, "Toplam Se\xE7ilen \xDCr\xFCn Say\u0131s\u0131 : ", selectedProducts.length), /*#__PURE__*/React.createElement("div", null, product.map(function (product, index) {
+    return /*#__PURE__*/React.createElement("div", {
+      key: index
+    }, /*#__PURE__*/React.createElement("h2", null, product.name), product.price, /*#__PURE__*/React.createElement("button", {
+      type: "button",
+      id: index,
+      onClick: function onClick(event) {
+        return productAdd(event, product);
+      }
+    }, "\xDCr\xFCn Ekle"));
+  }))); /* bu yazım şekline JSX : JSON XML denir. */
+  root.render(template);
+}
+renderApp();
--- Hello-React/src/app.js
+++ Hello-React/src/app.js
@@ -1,13 +1,54 @@
-var template = 
-<div>
-    <h1>My first react app2</h1>
-    <div>Lorem ipsum dolor sit amet.</div>
-    <ul>
-        <li>Lorem, ipsum dolor.</li>
-        <li>Lorem, ipsum dolor.</li>
-        <li>Lorem, ipsum dolor.</li>
-        <li>Lorem, ipsum dolor.</li>
-    </ul>
-</div> /* bu yazım şeklime JSX : JSON XML denir. */
 var root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(template)
+
+let product = [
+    {name:"Iphone11", price:20000},
+    {name:"Iphone12", price:30000}
+]
+
+let selectedProducts = []
+
+function productAdd(event,product){
+    console.log(event.target, product.name)
+    selectedProducts.push(product.name)
+    renderApp()
+}
+
+ function saveProduct(event){
+    event.preventDefault();/* submit edilen sayfanın refresh edilmesinin önüne geçer */
+    var pname = event.target.elements.productName.value
+    var pprice = event.target.elements.productPrice.value
+    var newProduct = {
+        name:pname,
+        price:pprice
+    }
+    product.push(newProduct)
+    event.target.elements.productName.value = ""
+    event.target.elements.productPrice.value = ""
+    renderApp()
+}
+
+function renderApp(){
+    var template = 
+    <div>
+        <form onSubmit={saveProduct}>
+            <input type="text" name="productName" id="productName" />
+            <input type="text" name="productPrice" id="productPrice" />
+            <button type="submit">Ekle</button>
+        </form>
+
+        <h1>My first react app</h1>
+        <h2>Toplam Seçilen Ürün Sayısı : {selectedProducts.length}</h2>
+        <div >
+            { product.map((product,index) => 
+                <div key={index}>
+                    {<h2>{product.name}</h2>}
+                    {product.price}   
+                    <button type="button" id={index} onClick={(event) => productAdd(event,product)}>Ürün Ekle</button>   
+                </div>
+            )}     
+        </div>
+    </div> /* bu yazım şekline JSX : JSON XML denir. */
+    root.render(template)
+}
+
+renderApp()
